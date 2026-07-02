9d372ae8cc02c193eaa0f1705aa76fd6be2fbb82
1F10250299 <s1f102502991@iniad.org>
2026-07-02 12:08:00 +0900

    viewsを修正

--- config/urls.py
+++ config/urls.py
@@ -22,5 +22,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', todo_views.index, name='index'),
     path('<int:task_id>/', todo_views.detail, name='detail'),
-    path('<int:task_id>/update', todo_viwes.update, name='update'),
+    path('<int:task_id>/update', todo_views.update, name='update'),
 ]
